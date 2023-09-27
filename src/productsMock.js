bff69779dd9cdaccff7ac7abc503a25c29b44ebb
MateoMigone <mateomigone@gmail.com>
2023-09-27 14:42:20 -0300

    agregando el productsMock.js

---
+++ src/productsMock.js
@@ -0,0 +1,81 @@
+export const products = [
+  {
+    id: 1,
+    title: "Camiseta Suplente Argentina 1986",
+    price: 25000,
+    stock: 15,
+    description:
+      "Camiseta suplente que usó la selección Argentina durante el mundial de México 1986",
+    category: "Camiseta",
+    img: "https://http2.mlstatic.com/D_748674-MLA44184283584_112020-F.jpg",
+  },
+  {
+    id: 2,
+    title: "Camiseta Titular Barcelona 2006/07",
+    price: 25000,
+    stock: 20,
+    description:
+      "Camiseta titular que usó el FC Barcelona en la temporada 2006/07",
+    category: "Camiseta",
+    img: "https://i.ebayimg.com/images/g/IEQAAOSw0hBggXiV/s-l1600.jpg",
+  },
+  {
+    id: 3,
+    title: "Camiseta Titular Arsenal 2002/03",
+    price: 25000,
+    stock: 10,
+    description: "Camiseta titular que usó el Arsenal en la temporada 2002/03",
+    category: "Camiseta",
+    img: "https://lamaquinaretro.com.ar/wp-content/uploads/2022/05/ars.jpg",
+  },
+  {
+    id: 4,
+    title: "Camiseta Titular Milan 2006/07 Manga Larga",
+    price: 25000,
+    stock: 15,
+    description:
+      "Camiseta manga larga titular que usó el Milan en la temporada 2006/07",
+    category: "Camiseta",
+    img: "https://cf.camisetasfutbol.com.cn/upload/ttmall/img/20210325/655232424993d97b26381ef964f80c20.png",
+  },
+  {
+    id: 5,
+    title: "Camiseta Titular Manchester United 2007/08",
+    price: 25000,
+    stock: 18,
+    description:
+      "Camiseta titular que usó el Manchester United en la temporada 2007/08",
+    category: "Camiseta",
+    img: "https://tecalzoshoes.com/wp-content/uploads/2023/07/4A2395B9-A3EB-4C9A-BB80-8DB1A3AC6752.jpeg",
+  },
+  {
+    id: 7,
+    title: "Camiseta Titular River Plate 1996",
+    price: 25000,
+    stock: 20,
+    description:
+      "Camiseta titular que usó River Plate campeón de la copa libertadores en 1996",
+    category: "Camiseta",
+    img: "https://trizop.com/wp-content/uploads/2021/04/riverplate-9596-retro-trizop-1.webp",
+  },
+  {
+    id: 7,
+    title: "Camiseta Titular River Plate 1986",
+    price: 25000,
+    stock: 20,
+    description:
+      "Camiseta titular que usó River Plate campeón de la copa libertadores en 1986",
+    category: "Camiseta",
+    img: "https://cdn.shopify.com/s/files/1/0567/2907/5873/products/WhatsAppImage2021-05-13at14.25.58.jpg",
+  },
+  {
+    id: 8,
+    title: "Camiseta Titular Real Madrid 2010/11",
+    price: 25000,
+    stock: 15,
+    description:
+      "Camiseta titular que usó el Real Madrid en la temporada 2010/11",
+    category: "Camiseta",
+    img: "https://www.merchandisingplaza.ca/101076/2/Jerseys-Real-Madrid-2010-11-Real-Madrid-Adidas-Champions-League-Home-Shirt-l.jpg",
+  },
+];
