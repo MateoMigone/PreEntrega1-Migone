f549713809697ab0001681bc91eb002e8c567852
MateoMigone <mateomigone@gmail.com>
2023-10-25 10:43:29 -0300

    adding firebase config

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC3Ntay1Wfke3HpXSH4q2mRdpe8UMvPVaw",
+  authDomain: "futbol-retro.firebaseapp.com",
+  projectId: "futbol-retro",
+  storageBucket: "futbol-retro.appspot.com",
+  messagingSenderId: "116166879119",
+  appId: "1:116166879119:web:b68136b13026fd12650dfa",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
